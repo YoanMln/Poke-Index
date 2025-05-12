90c2788e5e367bc03616ad203d1fbeebad544b19
YoanMln <yoan.moulin@hotmail.fr>
2025-05-12 21:14:02 +0200

    Structure JS history-display

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const pokeHistory = [];
+const maxHistory = 4;
+
 const url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
 
 function loadNextPoke() {
@@ -22,10 +25,41 @@ function loadNextPoke() {
       container.appendChild(nameElement);
       container.appendChild(typeElement);
       container.appendChild(imageElement);
+
+      const currentPoke = {
+        name: data.name,
+        type: data.types[0].type.name,
+        image: data.sprites.front_default,
+      };
+      pokeHistory.unshift(currentPoke);
+
+      if (pokeHistory.length > maxHistory) {
+        pokeHistory.pop();
+      }
+      displayHistory();
     })
+
     .catch((error) => {
       console.error(`Erreur :`, error);
     });
+}
+
+function displayHistory() {
+  const containerDisplay = document.querySelector(`.historyDisplay`);
+  containerDisplay.innerHTML = "";
+
+  pokeHistory.forEach((poke) => {
+    const card = document.createElement("div");
+    card.classList.add("history-card");
+
+    card.innerHTML = `
+        <h4>${poke.name}</h4>
+        <img src="${poke.image}" alt="${poke.name}" width="80">
+        <p>Type : ${poke.type}</p>
+      `;
+
+    containerDisplay.appendChild(card);
+  });
 }
 
 loadNextPoke();
